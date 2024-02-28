41927381168679d29e2ae7e1c278e22bf253d09d
Xmyxiemy <103745244+Xmyxiemy@users.noreply.github.com>
2024-02-29 01:33:56 +0800

    Create block.py

---
+++ rtdet/nn/modules/block.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+import torch
+
+
+class DFL(nn.Module):
+    """
+    Integral module of Distribution Focal Loss (DFL).
+
+    Proposed in Generalized Focal Loss https://ieeexplore.ieee.org/document/9792391
+    """
+
+    def __init__(self, c1=16):
+        """Initialize a convolutional layer with a given number of input channels."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, 1, 1, bias=False).requires_grad_(False)
+        x = torch.arange(c1, dtype=torch.float)
+        self.conv.weight.data[:] = nn.Parameter(x.view(1, c1, 1, 1))
+        self.c1 = c1
+
+    def forward(self, x):
+        return self.conv(x.unflatten(1, [4, -1]).transpose(2, 1).softmax(1)).squeeze(1)
+        
